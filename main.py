56aff0ac2eb0f0f8db3598a469a0ec9830bbdada
FrancescoCesari97 <freedus97@gmail.com>
2024-07-04 13:00:58 +0200

    added constant and function

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import random
+
+
+GAME_WIDTH = 750
+GAME_HEIGHT = 750
+SPEED = 50
+SPACE_SIZE = 40
+BODY_PART = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
